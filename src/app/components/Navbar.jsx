b440a3c5c388aa1cbaa50720c8be7f4cc74b156a
Tiyasha Ghosh <gtiyasha274@gmail.com>
2025-06-18 11:22:17 +0530

    🖋️ Updated logo to TG in HeroSection and Footer

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -25,7 +25,7 @@ const Navbar = () => {
   return (
     <nav className="fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-[rgba(18,18,18,0.6)] backdrop-blur-md">
         <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
-        <Link href={"/"} className="text-2xl md:text-5xl text-white font-semibold">LOGO</Link>
+        <Link href={"/"} className="text-2xl md:text-5xl text-white font-semibold">TG</Link>
         <div className="mobile-menu block md:hidden">
           {
             !navbarOpen?(
